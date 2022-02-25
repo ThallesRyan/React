499f05058b9f957722daf7fb17f9493f00460461
ThallesRyan <thallesryan2@gmail.com>
2022-02-25 19:30:31 -0300

    Calculator

--- curso-react/calculadora/src/main/Calculator.jsx
+++ curso-react/calculadora/src/main/Calculator.jsx
@@ -1,2 +1,13 @@
 import React, { Component} from 'react'
+import './Calculator.css'
 
+// componente de classe, pois irá usar estado...
+export default class Calculator extends Component{
+    render(){
+        return(
+            <div className='calculator'>
+
+            </div>
+        )
+    }
+}
